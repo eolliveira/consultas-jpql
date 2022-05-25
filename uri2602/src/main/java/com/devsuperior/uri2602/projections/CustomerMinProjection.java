d0d27b075c5ab4b71b7a8c346163c397e68aeb47
unknown <erick.oliveira9607@gmail.com>
2022-05-25 00:14:13 -0300

    Implements uri2602 - SQL native

---
+++ uri2602/src/main/java/com/devsuperior/uri2602/projections/CustomerMinProjection.java
@@ -0,0 +1,9 @@
+package com.devsuperior.uri2602.projections;
+
+public interface CustomerMinProjection {
+	
+	//Projeção minima de Customer, obtendo apenas o nome da consulta SQL
+	
+	String getName();
+
+}
