ddcb47cc473d712586c29f21020c0a233a480382
unknown <erick.oliveira9607@gmail.com>
2022-06-05 21:53:54 -0300

    Implements uri2990 - SQL native

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2990;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2990Application implements CommandLineRunner {
@@ -19,6 +24,15 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		System.out.println("Consulta SQL raiz");
+
+		List<EmpregadoDeptProjection> list = repository.employeesWithoutProject();
+		List<EmpregadoDeptDTO> list2 = list.stream().map(e -> new EmpregadoDeptDTO(e)).collect(Collectors.toList());
+
+		for(EmpregadoDeptDTO e : list2) {
+			System.out.println(e);
+		}
+
 	}
 }
--- uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
@@ -1,9 +1,23 @@
 package com.devsuperior.uri2990.repositories;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2990.entities.Empregado;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT e.cpf, e.enome, d.dnome " +
+            "FROM " +
+            "empregados e " +
+            "LEFT JOIN trabalha t ON e.cpf = t.cpf_emp " +
+            "JOIN departamentos d ON d.dnumero = e.dnumero " +
+            "WHERE t.pnumero IS null " +
+            "ORDER BY e.cpf")
+    List<EmpregadoDeptProjection> employeesWithoutProject();
 
 }
